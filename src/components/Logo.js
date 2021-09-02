51911ca5e2ed79c8b1ce33bde7a4a8971dea3730
dando54 <dandoczi.denis@gmail.com>
2021-09-02 17:37:30 +0300

    initial logo commit

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './logo.module.scss';
+
+const Logo = () => {
+    const { logo } = styles;
+    return (
+        <p className={logo}>pe viitor</p>
+    )
+}
+
+export default Logo;
