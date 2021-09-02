dfed24e040f4b099ecbd8d7e6e6a0b3beb9167d3
Budaioana <buda_ioana2002@yahoo.com>
2021-09-02 14:13:55 +0300

    setup redux

---
+++ src/redux/constants/constants.js
@@ -0,0 +1,3 @@
+export const BACKGROUND_ON = "BACKGROUND_ON";
+
+export const BACKGROUND_OFF = "BACKGROUND_OFF";
--- src/screens/serp/serp.js
+++ src/screens/serp/serp.js
@@ -1,15 +1,7 @@
-import React from 'react';
+import React from "react";
 import "./serp.scss";
 
-
-
 const Serp = () => {
-
-
-    return (
-        <div>
-            Hello from search page
-        </div>
-    )
-}
+  return <div>Hello from search page</div>;
+};
 export default Serp;
