fec681ef2230f0bfbe62960e772fe753fa6589f6
Daneil Ungureanu <72557684+ungureanudaniel00@users.noreply.github.com>
2021-09-06 13:27:24 +0300

    Update filterOptions.js

--- src/redux/actions/filterOptions.js
+++ src/redux/actions/filterOptions.js
@@ -1,7 +1,6 @@
 import  {SET_FILTER_OPTIONS_DATA}  from "redux/constants/constants";
 
 export const setFilterOptionsData = (data) => {
-    //console.log(data);
   return {
     type: SET_FILTER_OPTIONS_DATA,
     payload: data,
