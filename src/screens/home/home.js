dd3002079d79305656cbdad7fb10eb47cd80b2aa
TudorBucur19 <tudor.bucur88@gmail.com>
2021-09-06 15:23:02 +0300

    pull-request 47 errors fix

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
+  faGlobeEurope,
+  faBuilding,
+  faMapMarkerAlt,
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
   switchBackgroundOn,
   switchBackgroundOff,
 } from '../../redux/actions/background';
 import Logo from 'components/Logo/Logo';
 import SearchBar from 'components/SearchBar/SearchBar';
 import SearchFilter from '../../components/SearchFilter/SearchFilter';
-import {
-  faGlobeEurope,
-  faBuilding,
-  faMapMarkerAlt,
-} from '@fortawesome/free-solid-svg-icons';
+
 import styles from './home.module.scss';
 
 const Home = (props) => {
