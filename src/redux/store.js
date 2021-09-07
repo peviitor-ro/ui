ee767ed1ccc0d792d94b947bcf00151f71788e84
Daniel <ungureanudaniel00@yahoo.com>
2021-09-07 15:19:21 +0300

    BR_60: made functionality for switching background. made call function for background images from API. installed react-slideshow-image library, need `npm install` for update package.json. removed unnecessary background toggle functionality from redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
-import { combineReducers, createStore } from 'redux';
-import { backgroundReducer } from './background';
-import searchResultsReducer from 'redux/reducers/searchResults';
-import filterOptionsReducer from 'redux/reducers/filterOptions';
-import currentFilterOptionReducer from './reducers/currentFilterOption';
+import { combineReducers, createStore } from "redux";
+import searchResultsReducer from "redux/reducers/searchResults";
+import filterOptionsReducer from "redux/reducers/filterOptions";
+import currentFilterOptionReducer from "./reducers/currentFilterOption";
 
 const reducer = combineReducers({
-  isBackground: backgroundReducer,
   searchResults: searchResultsReducer,
   filterOptions: filterOptionsReducer,
   currentFilterOption: currentFilterOptionReducer,
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,78 +1,60 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  switchBackgroundOn,
-  switchBackgroundOff,
-} from '../../redux/actions/background';
-import Logo from 'components/Logo/Logo';
-import SearchBar from 'components/SearchBar/SearchBar';
-import SearchFilter from '../../components/SearchFilter/SearchFilter';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Logo from "components/Logo/Logo";
+import SearchBar from "components/SearchBar/SearchBar";
+import SearchFilter from "../../components/SearchFilter/SearchFilter";
+import BackgroundSlider from "../../components/BackgroundSlider/BackgroundSlider";
 import {
   faGlobeEurope,
   faBuilding,
   faMapMarkerAlt,
-} from '@fortawesome/free-solid-svg-icons';
+} from "@fortawesome/free-solid-svg-icons";
 
 import {
   setCurrentCountryFilterOption,
   setCurrentCityFilterOption,
   setCurrentCompanyFilterOption,
-} from 'redux/actions/currentFilterOption';
-import styles from './home.module.scss';
+} from "redux/actions/currentFilterOption";
+import styles from "./home.module.scss";
 
-const Home = (props) => {
+const Home = () => {
   const dispatch = useDispatch();
-  const { isBackground } = useSelector((state) => state);
   const filterOptions = useSelector((state) => state.filterOptions);
   const currentFilterOption = useSelector((state) => state.currentFilterOption);
-  const { welcome, notWelcome, filtersContainer } = styles;
+  const { filtersContainer, homeContainer } = styles;
   return (
-    <>
-      <Logo />
-      <SearchBar />
-      <div className={filtersContainer}>
-        <SearchFilter
-          icon={faGlobeEurope}
-          text={currentFilterOption.country}
-          options={filterOptions.countries}
-          onSelectOption={(data) => {
-            dispatch(setCurrentCountryFilterOption(data));
-          }}
-        />
-        <SearchFilter
-          icon={faMapMarkerAlt}
-          text={currentFilterOption.city}
-          options={filterOptions.cities}
-          onSelectOption={(data) => dispatch(setCurrentCityFilterOption(data))}
-        />
-        <SearchFilter
-          icon={faBuilding}
-          text={currentFilterOption.company}
-          options={filterOptions.companies}
-          onSelectOption={(data) =>
-            dispatch(setCurrentCompanyFilterOption(data))
-          }
-        />
-      </div>
-      <div className={isBackground ? `${welcome}` : `${notWelcome}`}>
-        <div className="inside"> Hello from home </div>
-        Hello from home
-        <button
-          onClick={() => {
-            dispatch(switchBackgroundOn());
-          }}
-        >
-          Put this on
-        </button>
-        <button
-          onClick={() => {
-            dispatch(switchBackgroundOff());
-          }}
-        >
-          Put this off
-        </button>
-      </div>
-    </>
+    <div className={homeContainer}>
+      <BackgroundSlider>
+        <Logo />
+        <SearchBar />
+        <div className={filtersContainer}>
+          <SearchFilter
+            icon={faGlobeEurope}
+            text={currentFilterOption.country}
+            options={filterOptions.countries}
+            onSelectOption={(data) => {
+              dispatch(setCurrentCountryFilterOption(data));
+            }}
+          />
+          <SearchFilter
+            icon={faMapMarkerAlt}
+            text={currentFilterOption.city}
+            options={filterOptions.cities}
+            onSelectOption={(data) =>
+              dispatch(setCurrentCityFilterOption(data))
+            }
+          />
+          <SearchFilter
+            icon={faBuilding}
+            text={currentFilterOption.company}
+            options={filterOptions.companies}
+            onSelectOption={(data) =>
+              dispatch(setCurrentCompanyFilterOption(data))
+            }
+          />
+        </div>
+      </BackgroundSlider>
+    </div>
   );
 };
 export default Home;
