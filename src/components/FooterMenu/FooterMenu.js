00136d9c772d97f96f0cf710a122e72a3507ddb3
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2021-10-31 14:59:36 +0200

    fix warnings step 2
    fix warnings step 2

--- src/components/FooterMenu/FooterMenu.js
+++ src/components/FooterMenu/FooterMenu.js
@@ -1,14 +1,12 @@
-import Hamburger from 'hamburger-react';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from 'components/FooterMenu/footerMenu.module.scss';
 import { useSelector } from 'react-redux';
-import { faFileExcel } from '@fortawesome/free-solid-svg-icons';
 import useWindowDimensions from 'utils/customsHooks/useWindowsDimensions';
 
 const FooterMenu = () => {
     const { isBurger } = useSelector(state => state)
     const { footerMenuContainer } = styles;
-    const { height, width } = useWindowDimensions();
+    const { width } = useWindowDimensions();
     const footerStyle = {
         display: (isBurger || width > 768) ? 'block' : 'none',
 
