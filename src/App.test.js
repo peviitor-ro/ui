0a5667836669fb7081ba4e2da320c863fe5e2d53
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2021-11-01 09:14:26 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@
 
  test('renders learn react link', () => {
    render(<App />);
-   const linkElement = screen.getByText(/learn react/i);
+   const linkElement = screen.getAllByText(/pe viitor/i);
    expect(linkElement).toBeInTheDocument();
  });
